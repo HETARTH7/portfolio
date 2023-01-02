c3275d24e883673cdc787b0971258d3f9ffaedce
unknown <hetarth002@gmail.com>
2023-01-02 12:56:42 +0530

    add nav-icon for mobile view

--- pages/navbar.js
+++ pages/navbar.js
@@ -1,22 +1,23 @@
 import Link from "next/link";
 
 const Navbar = () => {
+  const icon = () => {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  };
   return (
-    <nav>
-      <Link className="nav-links" href="/">
-        Home
-      </Link>
-      <Link className="nav-links" href="/projects">
-        Projects
-      </Link>
-      <Link className="nav-links" href="/resume">
-        Resume
-      </Link>
-      <Link className="nav-links" href="/blog">
-        Blog
-      </Link>
-      <Link className="nav-links" href="/contact">
-        Contact Me
+    <nav className="topnav" id="myTopnav">
+      <Link href="/">Home</Link>
+      <Link href="/projects">Projects</Link>
+      <Link href="/resume">Resume</Link>
+      <Link href="/blog">Blog</Link>
+      <Link href="/contact">Contact Me</Link>
+      <Link href="/" className="icon" onClick={icon}>
+        <i className="fa fa-bars"></i>
       </Link>
     </nav>
   );
