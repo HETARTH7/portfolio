4009fe6c6356c390c45ce60dbb370b728ce911ba
unknown <hetarth002@gmail.com>
2023-01-03 12:05:33 +0530

    add about page

--- pages/about.js
+++ pages/about.js
@@ -1,65 +1,65 @@
 import Navbar from "./navbar";
 import Footer from "./footer";
+
 const About = () => {
   return (
     <div>
       <Navbar />
       <div id="about">
-        <h1>About</h1>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Habitant
-          morbi tristique senectus et netus et malesuada fames ac. Quam
-          elementum pulvinar etiam non. Euismod elementum nisi quis eleifend
-          quam adipiscing vitae proin. Porttitor lacus luctus accumsan tortor
-          posuere ac ut consequat. Massa placerat duis ultricies lacus sed.
-          Lectus magna fringilla urna porttitor rhoncus dolor purus non. In
-          aliquam sem fringilla ut morbi tincidunt augue. Arcu dictum varius
-          duis at consectetur lorem donec massa sapien. Venenatis urna cursus
-          eget nunc scelerisque viverra mauris in. Morbi leo urna molestie at
-          elementum eu. Ornare aenean euismod elementum nisi quis eleifend quam
-          adipiscing. Vel elit scelerisque mauris pellentesque pulvinar
-          pellentesque. Imperdiet nulla malesuada pellentesque elit eget gravida
-          cum. Nulla malesuada pellentesque elit eget gravida cum sociis natoque
-          penatibus. Vitae proin sagittis nisl rhoncus mattis. Sodales neque
-          sodales ut etiam sit amet nisl. In pellentesque massa placerat duis
-          ultricies lacus sed. Consequat nisl vel pretium lectus quam id leo in
-          vitae. Magnis dis parturient montes nascetur ridiculus mus mauris.
-          Turpis egestas integer eget aliquet nibh. Senectus et netus et
-          malesuada fames ac turpis egestas maecenas. At quis risus sed
-          vulputate odio ut enim blandit. Quam pellentesque nec nam aliquam.
-          Diam vulputate ut pharetra sit amet aliquam. Donec adipiscing
-          tristique risus nec feugiat in fermentum posuere urna. Eget felis eget
-          nunc lobortis mattis aliquam faucibus. Fringilla ut morbi tincidunt
-          augue. Varius quam quisque id diam vel. Enim diam vulputate ut
-          pharetra sit amet aliquam. Tempus quam pellentesque nec nam aliquam
-          sem et. In egestas erat imperdiet sed euismod. Commodo quis imperdiet
-          massa tincidunt nunc pulvinar sapien. Justo eget magna fermentum
-          iaculis eu non diam phasellus vestibulum. Nulla posuere sollicitudin
-          aliquam ultrices sagittis orci. Duis at tellus at urna condimentum
-          mattis pellentesque id nibh. Sem integer vitae justo eget magna
-          fermentum iaculis eu. Sed enim ut sem viverra aliquet eget sit amet
-          tellus. Et molestie ac feugiat sed lectus. Tellus mauris a diam
-          maecenas sed enim ut sem viverra. Nisl suscipit adipiscing bibendum
-          est. Pulvinar elementum integer enim neque volutpat ac tincidunt
-          vitae. Mi ipsum faucibus vitae aliquet. Arcu non odio euismod lacinia.
-          Sit amet venenatis urna cursus. Vestibulum rhoncus est pellentesque
-          elit ullamcorper dignissim. Dictum fusce ut placerat orci nulla
-          pellentesque dignissim. Diam in arcu cursus euismod quis viverra. In
-          cursus turpis massa tincidunt dui ut ornare lectus sit. Elementum nibh
-          tellus molestie nunc non. Tortor condimentum lacinia quis vel eros
-          donec ac. Eleifend mi in nulla posuere. Gravida arcu ac tortor
-          dignissim. Vestibulum lorem sed risus ultricies tristique. Duis
-          convallis convallis tellus id interdum velit laoreet. Consectetur
-          adipiscing elit ut aliquam purus sit amet. Quis ipsum suspendisse
-          ultrices gravida dictum. Elementum sagittis vitae et leo duis ut diam.
-          Vivamus at augue eget arcu dictum varius duis. Lectus nulla at
-          volutpat diam ut. Gravida neque convallis a cras semper auctor neque
-          vitae. Morbi tristique senectus et netus. Eros in cursus turpis massa
-          tincidunt dui ut ornare. Sem viverra aliquet eget sit amet tellus cras
-          adipiscing. Tortor at auctor urna nunc. At tempor commodo ullamcorper
-          a lacus. Pulvinar pellentesque habitant morbi tristique senectus.
-        </p>
+        <div className="up">
+          <div className="aboutme">
+            <h2 className="head">I&apos;m Hetarth,</h2>
+            <h2 className="head">a web developer.</h2>
+            <p className="text">
+              Hello there! I am a Computer Science undergradute at
+            </p>
+            <p className="text">Vellore Institute of Technology, Vellore.</p>
+            <p className="text">
+              I&apos;m a passionate web developer and am trying to
+            </p>
+            <p className="text">level up my UX/UI designing skills.</p>
+          </div>
+
+          <div className="skills">
+            <h1 className="head">Skills</h1>
+            <ul>
+              <li>Front End Development</li>
+              <li>Javascript</li>
+              <li>ReactJS</li>
+              <li>NextJS</li>
+              <li>Back End Development</li>
+              <li>Python</li>
+              <li>PHP</li>
+              <li>SQL, NOSQL</li>
+              <li>NodeJS</li>
+              <li>ExpressJS</li>
+              <li>MongoDB</li>
+              <li>MySQL</li>
+              <li>C/C++</li>
+              <li>Java</li>
+            </ul>
+          </div>
+        </div>
+        <div className="down">
+          <div className="interests">
+            <h1 className="head">I like</h1>
+            <ul>
+              <li className="text">Football</li>
+              <li className="text">Star Wars</li>
+              <li className="text">Pop Music and Classic Rock</li>
+              <li className="text">Sketching</li>
+            </ul>
+          </div>
+          <div className="learning">
+            <h1 className="head">I&apos;m learning</h1>
+            <ul>
+              <li className="text">Machine Learning</li>
+              <li className="text">French</li>
+              <li className="text">Spanish</li>
+              <li className="text">Light Saber fighting</li>
+            </ul>
+          </div>
+        </div>
       </div>
       <Footer />
     </div>
--- pages/footer.js
+++ pages/footer.js
@@ -4,25 +4,44 @@ const Footer = () => {
   return (
     <div id="contact footer">
       <h1>Contact</h1>
-      <Link className="link" href="https://www.instagram.com/hetarth.7/">
+      <Link
+        target="_blank"
+        rel="noopener noreferrer"
+        className="link"
+        href="https://www.instagram.com/hetarth.7/"
+      >
         Instagram
       </Link>
       <Link
+        target="_blank"
+        rel="noopener noreferrer"
         className="link"
         href="https://www.linkedin.com/in/hetarth-raval-936462218/"
       >
         Linkedin
       </Link>
-      <Link className="link" href="https://github.com/HETARTH7">
+      <Link
+        target="_blank"
+        rel="noopener noreferrer"
+        className="link"
+        href="https://github.com/HETARTH7"
+      >
         GitHub
       </Link>
       <Link
+        target="_blank"
+        rel="noopener noreferrer"
         className="link"
         href="https://stackoverflow.com/users/16973267/hetarth7"
       >
         Stackoverflow
       </Link>
-      <Link className="link" href="mailto:hetarth002@gmail.com">
+      <Link
+        target="_blank"
+        rel="noopener noreferrer"
+        className="link"
+        href="mailto:hetarth002@gmail.com"
+      >
         Email
       </Link>
     </div>
