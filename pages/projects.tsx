1b3eb9b588f54b69a241f5b91569654479bc9dea
unknown <hetarth002@gmail.com>
2023-02-14 00:16:41 +0530

    add connect

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,6 +1,6 @@
 export default function Projects() {
   return (
-    <div id="projects" className="h-screen text-center">
+    <div id="projects" className="h-screen text-center font-mono">
       <h1 className="text-8xl font-black tracking-tighter pt-32">Projects</h1>
       <h1 className="text-4xl font-black tracking-tighter mt-16">XrossFit</h1>
       <p className="text-xl text-gray-400 tracking-tighter mt-8">MERN Stack</p>
--- pages/work.tsx
+++ pages/work.tsx
@@ -1,6 +1,6 @@
 export default function Work() {
   return (
-    <div id="work" className="h-screen text-center">
+    <div id="work" className="h-screen text-center font-mono">
       <h1 className="text-4xl font-black tracking-tighter pt-32">
         The Sparks Foundation
       </h1>
