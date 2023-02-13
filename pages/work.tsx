88c4b54b51a0a9518a0685a93d11e4f0d5df60aa
unknown <hetarth002@gmail.com>
2023-02-13 20:45:39 +0530

    add work

--- pages/work.tsx
+++ pages/work.tsx
@@ -1,7 +1,12 @@
 export default function Work() {
   return (
-    <div id="work" className="h-screen">
-      Work
+    <div id="work" className="h-screen text-center">
+      <h1 className="text-4xl font-black tracking-tighter pt-32">
+        The Sparks Foundation
+      </h1>
+      <p className="text-xl text-gray-400 tracking-tighter mt-8">
+        Web Development Intern<span className="ml-8">December 2022</span>
+      </p>
     </div>
   );
 }
