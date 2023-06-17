3fc61a7eef894ed31147f2fb8e9f5e46d3ed7286
unknown <hetarth002@gmail.com>
2023-06-17 21:54:09 +0530

    fix deployment bug a521bf2

--- app/page.tsx
+++ app/page.tsx
@@ -8,19 +8,17 @@ import Contact from "./contact";
 
 export default function Home() {
   return (
-    <ThemeProvider enableSystem={true} attribute="class">
-      <div>
-        <Navbar />
-        <div id="home" className="pt-32 ml-80 h-screen max-[600px]:ml-0">
-          <h1 className="text-2xl">HELLO THERE</h1>
-          <h1 className="text-6xl">I&apos;m Hetarth Raval</h1>
-          <h1 className="text-4xl">A Full Stack Developer</h1>
-        </div>
-        <About />
-        <Projects />
-        <Contact />
-        <Footer />
+    <div>
+      <Navbar />
+      <div id="home" className="pt-32 ml-80 h-screen max-[600px]:ml-0">
+        <h1 className="text-2xl">HELLO THERE</h1>
+        <h1 className="text-6xl">I&apos;m Hetarth Raval</h1>
+        <h1 className="text-4xl">A Full Stack Developer</h1>
       </div>
-    </ThemeProvider>
+      <About />
+      <Projects />
+      <Contact />
+      <Footer />
+    </div>
   );
 }
