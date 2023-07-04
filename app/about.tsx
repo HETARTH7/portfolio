c528a27cd27d461a0b56b9745b66b5c1e5cd1e03
unknown <hetarth002@gmail.com>
2023-07-04 19:45:49 +0530

    add dark mode icons and update contact page

--- app/about.tsx
+++ app/about.tsx
@@ -1,8 +1,17 @@
 import Image from "next/image";
 import { Pacifico } from "next/font/google";
+import { useEffect, useState } from "react";
+import { useTheme } from "next-themes";
 
 const pacifico = Pacifico({ weight: ["400"], subsets: ["latin"] });
 const About = () => {
+  const { systemTheme, theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+  if (!mounted) return null;
+  const currentTheme = theme === "system" ? systemTheme : theme;
   return (
     <div className="text-center mt-20 mb-44" id="about">
       <h1
@@ -25,256 +34,369 @@ const About = () => {
       </h1>
       <div className="mt-10">
         <Image
-          className="inline m-4"
+          className="inline m-3"
           src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg"
           height="40"
-          width="52"
+          width="40"
+          onClick={() => {
+            window.open(
+              "https://developer.mozilla.org/en-US/docs/Web/JavaScript",
+              "_blank"
+            );
+          }}
           alt="javascript logo"
         />
         <Image
-          className="inline m-4"
+          className="inline m-3"
           src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg"
           height="40"
-          width="52"
+          width="40"
+          onClick={() => {
+            window.open("https://www.typescriptlang.org/", "_blank");
+          }}
           alt="typescript logo"
         />
         <Image
-          className="inline m-4"
+          className="inline m-3"
           src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg"
           height="40"
-          width="52"
+          width="40"
+          onClick={() => {
+            window.open("https://react.dev/", "_blank");
+          }}
           alt="react logo"
         />
+        {currentTheme === "dark" ? (
+          <Image
+            className="inline m-3"
+            src="/next.svg"
+            height="40"
+            width="40"
+            onClick={() => {
+              window.open("https://nextjs.org/", "_blank");
+            }}
+            alt="nextjs logo"
+          />
+        ) : (
+          <Image
+            className="inline m-3"
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nextjs/nextjs-original.svg"
+            height="40"
+            width="40"
+            onClick={() => {
+              window.open("https://nextjs.org/", "_blank");
+            }}
+            alt="nextjs logo"
+          />
+        )}
         <Image
-          className="inline m-4"
-          src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nextjs/nextjs-original.svg"
-          height="40"
-          width="52"
-          alt="nextjs logo"
-        />
-        <Image
-          className="inline m-4"
+          className="inline m-3"
           src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original-wordmark.svg"
           height="40"
-          width="52"
+          width="40"
+          onClick={() => {
+            window.open("https://nodejs.org/en", "_blank");
+          }}
           alt="nodejs logo"
         />
         <Image
-          className="inline m-4"
+          className="inline m-3"
           src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/arduino/arduino-original.svg"
           height="40"
-          width="52"
+          width="40"
+          onClick={() => {
+            window.open("https://www.arduino.cc/", "_blank");
+          }}
           alt="arduino logo"
         />
         <Image
-          className="inline m-4"
+          className="inline m-3"
           height="40"
-          width="52"
+          width="40"
+          onClick={() => {
+            window.open("https://docs.oracle.com/en/java/", "_blank");
+          }}
           src="https://profilinator.rishav.dev/skills-assets/java-original-wordmark.svg"
           alt="Java"
         />
         <Image
-          className="inline m-4"
+          className="inline m-3"
           src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/babel/babel-original.svg"
           height="40"
-          width="52"
+          width="40"
+          onClick={() => {
+            window.open("https://babeljs.io/", "_blank");
+          }}
           alt="babel logo"
         />
         <Image
-          className="inline m-4"
+          className="inline m-3"
           src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg"
           height="40"
-          width="52"
+          width="40"
+          onClick={() => {
+            window.open("https://getbootstrap.com/", "_blank");
+          }}
           alt="bootstrap logo"
         />
         <Image
-          className="inline m-4"
+          className="inline m-3"
           src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/c/c-original.svg"
           height="40"
-          width="52"
+          width="40"
+          onClick={() => {
+            window.open("https://devdocs.io/c/", "_blank");
+          }}
           alt="c logo"
         />
         <Image
-          className="inline m-4"
+          className="inline m-3"
           src="https://profilinator.rishav.dev/skills-assets/cplusplus-original.svg"
           height="40"
-          width="52"
-          alt="c logo"
+          width="40"
+          onClick={() => {
+            window.open("https://devdocs.io/cpp/", "_blank");
+          }}
+          alt="cpp logo"
         />
         <Image
-          className="inline m-4"
+          className="inline m-3"
           src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original-wordmark.svg"
           height="40"
-          width="52"
+          width="40"
+          onClick={() => {
+            window.open("https://expressjs.com/", "_blank");
+          }}
           alt="express logo"
         />
         <Image
-          className="inline m-4"
+          className="inline m-3"
           src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/firebase/firebase-plain.svg"
           height="40"
-          width="52"
+          width="40"
+          onClick={() => {
+            window.open("https://firebase.google.com/", "_blank");
+          }}
           alt="firebase logo"
         />
         <Image
-          className="inline m-4"
+          className="inline m-3"
           src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/flask/flask-original.svg"
           height="40"
-          width="52"
+          width="40"
+          onClick={() => {
+            window.open(
+              "https://flask.palletsprojects.com/en/2.3.x/",
+              "_blank"
+            );
+          }}
           alt="flask logo"
         />
         <Image
-          className="inline m-4"
+          className="inline"
+          src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/pandas/pandas-original.svg"
+          height="40"
+          width="40"
+          onClick={() => {
+            window.open("https://pandas.pydata.org/", "_blank");
+          }}
+          alt="pandas logo"
+        />
+        <Image
+          className="inline m-3"
           src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg"
           height="40"
-          width="52"
+          width="40"
+          onClick={() => {
+            window.open("https://git-scm.com/", "_blank");
+          }}
           alt="git logo"
         />
         <Image
-          className="inline m-4"
-          src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg"
-          height="40"
-          width="52"
-          alt="github logo"
-        />
-        <Image
-          className="inline m-4"
+          className="inline m-3"
           src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg"
           height="40"
-          width="52"
+          width="40"
+          onClick={() => {
+            window.open(
+              "https://developer.mozilla.org/en-US/docs/Web/HTML",
+              "_blank"
+            );
+          }}
           alt="html5 logo"
         />
         <Image
-          className="inline m-4"
+          className="inline m-3"
           src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/heroku/heroku-original.svg"
           height="40"
-          width="52"
+          width="40"
+          onClick={() => {
+            window.open("https://www.heroku.com/", "_blank");
+          }}
           alt="heroku logo"
         />
         <Image
-          className="inline m-4"
+          className="inline m-3"
           src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jquery/jquery-original.svg"
           height="40"
-          width="52"
+          width="40"
+          onClick={() => {
+            window.open("https://jquery.com/", "_blank");
+          }}
           alt="jquery logo"
         />
         <Image
-          className="inline m-4"
+          className="inline m-3"
           src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jupyter/jupyter-original.svg"
           height="40"
-          width="52"
+          width="40"
+          onClick={() => {
+            window.open("https://jupyter.org/", "_blank");
+          }}
           alt="jupyter logo"
         />
         <Image
-          className="inline m-4"
+          className="inline m-3"
           src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/materialui/materialui-original.svg"
           height="40"
-          width="52"
+          width="40"
+          onClick={() => {
+            window.open("https://mui.com/", "_blank");
+          }}
           alt="materialui logo"
         />
         <Image
-          className="inline m-4"
+          className="inline m-3"
           src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/matlab/matlab-original.svg"
           height="40"
-          width="52"
+          width="40"
+          onClick={() => {
+            window.open(
+              "https://in.mathworks.com/products/matlab.html",
+              "_blank"
+            );
+          }}
           alt="matlab logo"
         />
         <Image
-          className="inline m-4"
+          className="inline m-3"
           src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg"
           height="40"
-          width="52"
+          width="40"
+          onClick={() => {
+            window.open("https://www.mongodb.com/", "_blank");
+          }}
           alt="mongodb logo"
         />
         <Image
-          className="inline m-4"
+          className="inline m-3"
           src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/php/php-original.svg"
           height="40"
-          width="52"
+          width="40"
+          onClick={() => {
+            window.open("https://www.php.net", "_blank");
+          }}
           alt="php logo"
         />
         <Image
-          className="inline m-4"
+          className="inline m-3"
           src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg"
           height="40"
-          width="52"
+          width="40"
+          onClick={() => {
+            window.open("https://www.python.org/", "_blank");
+          }}
           alt="python logo"
         />
         <Image
-          className="inline m-4"
+          className="inline m-3"
           src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/r/r-original.svg"
           height="40"
-          width="52"
+          width="40"
+          onClick={() => {
+            window.open("https://www.r-project.org/", "_blank");
+          }}
           alt="r logo"
         />
         <Image
-          className="inline m-4"
+          className="inline m-3"
           src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/redux/redux-original.svg"
           height="40"
-          width="52"
+          width="40"
+          onClick={() => {
+            window.open("https://redux.js.org/", "_blank");
+          }}
           alt="redux logo"
         />
         <Image
-          className="inline m-4"
-          src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/rstudio/rstudio-original.svg"
-          height="40"
-          width="52"
-          alt="rstudio logo"
-        />
-        <Image
-          className="inline m-4"
+          className="inline m-3"
           src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-original-wordmark.svg"
           height="40"
-          width="52"
+          width="40"
+          onClick={() => {
+            window.open("https://tailwindcss.com/", "_blank");
+          }}
           alt="tailwindcss logo"
         />
         <Image
-          className="inline m-4"
+          className="inline m-3"
           src="https://seaborn.pydata.org/_images/logo-mark-lightbg.svg"
           alt="seaborn"
           width="40"
-          height="52"
+          onClick={() => {
+            window.open("https://seaborn.pydata.org/", "_blank");
+          }}
+          height="40"
         />
         <Image
-          className="inline m-4"
+          className="inline m-3"
           src="https://www.vectorlogo.zone/logos/sqlite/sqlite-icon.svg"
           alt="sqlite"
           width="40"
-          height="52"
+          onClick={() => {
+            window.open("https://www.sqlite.org/index.html", "_blank");
+          }}
+          height="40"
         />
         <Image
-          className="inline m-4"
+          className="inline m-3"
           src="https://upload.wikimedia.org/wikipedia/commons/0/05/Scikit_learn_logo_small.svg"
           alt="scikit_learn"
           width="40"
-          height="52"
+          onClick={() => {
+            window.open("https://scikit-learn.org/stable", "_blank");
+          }}
+          height="40"
         />
         <Image
-          className="inline m-4"
+          className="inline m-3"
           src="https://www.vectorlogo.zone/logos/getpostman/getpostman-icon.svg"
           alt="postman"
           width="40"
-          height="52"
+          onClick={() => {
+            window.open("https://www.postman.com/", "_blank");
+          }}
+          height="40"
         />
         <Image
-          className="inline m-4"
-          src="/chakraui.png"
-          alt="postman"
-          width="40"
-          height="52"
-        />
-        <Image
-          className="inline m-4"
+          className="inline m-3"
           src="https://profilinator.rishav.dev/skills-assets/mysql-original-wordmark.svg"
-          alt="postman"
+          alt="mysql"
           width="40"
-          height="52"
+          onClick={() => {
+            window.open("https://www.mysql.com/", "_blank");
+          }}
+          height="40"
         />
         <Image
-          className="inline m-4"
-          src="/xampp.png"
-          alt="postman"
+          className="inline m-3"
+          src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-original.svg"
+          alt="docker"
           width="40"
-          height="52"
+          onClick={() => {
+            window.open("https://www.docker.com/", "_blank");
+          }}
+          height="40"
         />
       </div>
     </div>
--- app/contact.tsx
+++ app/contact.tsx
@@ -1,7 +1,8 @@
 import Image from "next/image";
-import React, { useState, useRef, LegacyRef } from "react";
+import React, { useState, useEffect } from "react";
 import emailjs from "@emailjs/browser";
 import { Pacifico } from "next/font/google";
+import { useTheme } from "next-themes";
 
 const pacifico = Pacifico({ weight: ["400"], subsets: ["latin"] });
 
@@ -9,6 +10,14 @@ const Contact = () => {
   const [fullName, setFullName] = useState("");
   const [email, setEmail] = useState("");
   const [message, setMessage] = useState("");
+
+  const { systemTheme, theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+  if (!mounted) return null;
+  const currentTheme = theme === "system" ? systemTheme : theme;
 
   const handleSubmit = (e: any) => {
     e.preventDefault();
@@ -120,7 +129,7 @@ const Contact = () => {
           />
         </svg>
 
-        <svg
+        {currentTheme==="light"?<svg
           className="inline m-4 hover:animate-bounce"
           width="50px"
           height="40px"
@@ -137,7 +146,25 @@ const Contact = () => {
             d="M8 1C4.133 1 1 4.13 1 7.993c0 3.09 2.006 5.71 4.787 6.635.35.064.478-.152.478-.337 0-.166-.006-.606-.01-1.19-1.947.423-2.357-.937-2.357-.937-.319-.808-.778-1.023-.778-1.023-.635-.434.048-.425.048-.425.703.05 1.073.72 1.073.72.624 1.07 1.638.76 2.037.582.063-.452.244-.76.444-.935-1.554-.176-3.188-.776-3.188-3.456 0-.763.273-1.388.72-1.876-.072-.177-.312-.888.07-1.85 0 0 .586-.189 1.924.716A6.711 6.711 0 018 4.381c.595.003 1.194.08 1.753.236 1.336-.905 1.923-.717 1.923-.717.382.963.142 1.674.07 1.85.448.49.72 1.114.72 1.877 0 2.686-1.638 3.278-3.197 3.45.251.216.475.643.475 1.296 0 .934-.009 1.688-.009 1.918 0 .187.127.404.482.336A6.996 6.996 0 0015 7.993 6.997 6.997 0 008 1z"
             clipRule="evenodd"
           />
-        </svg>
+        </svg>:
+        <svg
+          className="inline m-4 hover:animate-bounce"
+          width="50px"
+          height="40px"
+          viewBox="0 0 16 16"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          onClick={() => {
+            window.open("https://github.com/HETARTH7", "_blank");
+          }}
+        >
+          <path
+            fill="#fff"
+            fillRule="evenodd"
+            d="M8 1C4.133 1 1 4.13 1 7.993c0 3.09 2.006 5.71 4.787 6.635.35.064.478-.152.478-.337 0-.166-.006-.606-.01-1.19-1.947.423-2.357-.937-2.357-.937-.319-.808-.778-1.023-.778-1.023-.635-.434.048-.425.048-.425.703.05 1.073.72 1.073.72.624 1.07 1.638.76 2.037.582.063-.452.244-.76.444-.935-1.554-.176-3.188-.776-3.188-3.456 0-.763.273-1.388.72-1.876-.072-.177-.312-.888.07-1.85 0 0 .586-.189 1.924.716A6.711 6.711 0 018 4.381c.595.003 1.194.08 1.753.236 1.336-.905 1.923-.717 1.923-.717.382.963.142 1.674.07 1.85.448.49.72 1.114.72 1.877 0 2.686-1.638 3.278-3.197 3.45.251.216.475.643.475 1.296 0 .934-.009 1.688-.009 1.918 0 .187.127.404.482.336A6.996 6.996 0 0015 7.993 6.997 6.997 0 008 1z"
+            clipRule="evenodd"
+          />
+        </svg>}
 
         <svg
           className="inline m-4 hover:animate-bounce"
