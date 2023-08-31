16599d97da66ae1f27b8ee610aee7ac96cb4ab3a
unknown <hetarth002@gmail.com>
2023-08-31 17:51:43 +0530

    update footer

--- app/footer.tsx
+++ app/footer.tsx
@@ -1,4 +1,20 @@
+import Link from "next/link";
+
 const Footer = () => {
-  return <div className="text-center">Made with NextJS and Tailwind</div>;
+  const date = new Date();
+  const currentYear = date.getFullYear();
+  return (
+    <div className="text-center pb-7">
+      <div>&copy; {currentYear} Hetarth</div>
+      Powered by{" "}
+      <Link href="https://nextjs.org/" className="text-slate-400">
+        NextJS
+      </Link>{" "}
+      and{" "}
+      <Link className="text-slate-400" href="https://tailwindcss.com/">
+        Tailwind
+      </Link>
+    </div>
+  );
 };
 export default Footer;
